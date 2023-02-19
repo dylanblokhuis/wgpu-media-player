dfc5edc5c14f7e6742e662957cf8ea0f6aac6007
dylanblokhuis <dylanblokhuis3@hotmail.com>
2023-02-19 22:34:05 +0100

    also move vertex

--- src/renderer.rs
+++ src/renderer.rs
@@ -3,7 +3,7 @@ use std::num::NonZeroU32;
 use wgpu::util::DeviceExt;
 use winit::dpi::PhysicalSize;
 
-use crate::{texture::Texture, Vertex};
+use crate::texture::Texture;
 
 pub const INDICES: &[u16] = &[0, 1, 2, 3, 4, 5];
 
@@ -218,5 +218,34 @@ impl VideoRenderer {
                 tex_coords: [1.0, 0.0],
             },
         ]
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    tex_coords: [f32; 2], // NEW!
+}
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        use std::mem;
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x2, // NEW!
+                },
+            ],
+        }
     }
 }
